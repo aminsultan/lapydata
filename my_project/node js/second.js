c677d547820eafdc6d11976f6817d601c8da763d
aminsultan <aminsultan10@gmail.com>
2021-12-21 08:21:47 +0530

    4th commit

--- my_project/node js/second.js
+++
@@ -1,7 +0,0 @@
-var abc = require("E:/full stack development/my project/node js/first"); // we are calling first.js module
-
-console.log("val: "+abc.myVariable);
-
-console.log("add: " + abc.add(10, 5));
-
-console.log("hey:" +abc.myfunc());
